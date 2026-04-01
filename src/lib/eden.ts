9ba39938265287cdb3aaef4b4fc933ade9528e4a
Alfat Ardiansa <alfat.ardi3105@gmail.com>
2026-04-01 16:17:03 +0700

    Fix eden

--- src/lib/eden.ts
+++ src/lib/eden.ts
@@ -1,9 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import { apiRoutes } from "@/modules/v1/route";
+import { env } from "./env";
 
 // .api to enter /api prefix
 export const api =
   // process is defined on server side and build time
   typeof process !== "undefined"
-    ? treaty<typeof apiRoutes>("localhost:3000").api
+    ? treaty<typeof apiRoutes>(new URL(env.BASE_URL).host).api
     : treaty<typeof apiRoutes>("localhost:3000").api;
