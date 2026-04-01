84e423b15373d43c27a05dc4131dae63a33b2faa
Alfat Ardiansa <alfat.ardi3105@gmail.com>
2026-04-01 16:58:33 +0700

    Move API route out of API modules

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,4 +1,7 @@
-import { apiRoutes } from "@/modules/v1/route";
+import { apiV1Routes } from "@/modules/v1/route";
+import Elysia from "elysia";
+
+export const apiRoutes = new Elysia({ prefix: "/api" }).use(apiV1Routes);
 
 export const GET = apiRoutes.fetch,
   POST = apiRoutes.fetch,
--- src/modules/v1/route.ts
+++ src/modules/v1/route.ts
@@ -2,5 +2,3 @@ import Elysia from "elysia";
 import { linkRoutes } from "./link/route";
 
 export const apiV1Routes = new Elysia({ prefix: "/v1" }).use(linkRoutes);
-
-export const apiRoutes = new Elysia({ prefix: "/api" }).use(apiV1Routes);
